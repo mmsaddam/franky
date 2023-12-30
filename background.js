433d4ffb87a0f3b12ed1110995d4b4725e17f42c
Muzahidul Islam <mmsaddam@gmail.com>
2023-12-30 10:33:58 +0600

    Clean up

--- background.js
+++ background.js
@@ -94,9 +94,8 @@ async function sendRequest(messageArray) {
   
   // get the API model from local storage
   let apiModel = await new Promise(resolve => chrome.storage.local.get(['apiModel'], result => resolve(result.apiModel)));
-  console.log(apiModel);
-
-
+  // console.log(apiModel);
+  // console.log(messageArray);
   try {
       // send the request containing the messages to the OpenAI API
       let response = await fetch('https://api.openai.com/v1/chat/completions', {
@@ -118,11 +117,12 @@ async function sendRequest(messageArray) {
 
       // get the data from the API response as json
       let data = await response.json();
-
+      // console.log(data);
       // check if the API response contains an answer
       if (data && data.choices && data.choices.length > 0) {
           // get the answer from the API response
           let response = data.choices[0].message.content;
+          console.log(response);
           return response
           
       }
@@ -141,53 +141,18 @@ chrome.runtime.onMessage.addListener(({ summarize, error }) => {
   }
 });
   
-
-
 chrome.contextMenus.onClicked.addListener(async function (info) {
-        // let message = "Summarize the following texts: \n" + info.selectionText;
-        let message = "Who is Stev Jobs";
-        // chrome.runtime.sendMessage({ input: message })
-        chrome.runtime.sendMessage({ summarize: message });
-    // }
+    let message = "Summarize the following texts within 200 characters: \n" + info.selectionText;
+    let messageArray = [{ role: "user", "content": message }];
+    try {
+      sendRequest(messageArray);
+    } catch (error) {
+      console.log(error);
+    }
 });
 
-// // A generic onclick callback function.
-//  function genericOnClick(info) {
-//     if (info.selectionText) {
-//         console.log(info.selectionText);
-//         let message = "Summarize the following texts: \n" + info.selectionText;
-//         chrome.runtime.sendMessage({ input: message }, { 
-
-//         });
-//     }
-    
-//   switch (info.menuItemId) {
-//     case 'radio':
-//       // Radio item function
-//       console.log('Radio item clicked. Status:', info.checked);
-//       break;
-//     case 'checkbox':
-//       // Checkbox item function
-//       console.log('Checkbox item clicked. Status:', info.checked);
-//       break;
-//     default:
-//       // Standard context menu item function
-//     //   console.log('Standard context menu item clicked.');
-//   }
-// }
-
-
+/// Selection option added
 chrome.runtime.onInstalled.addListener(function () {
-  // Create one test item for each context type.
-//   let contexts = [
-//     'page',
-//     'selection',
-//     'link',
-//     'editable',
-//     'image',
-//     'video',
-//     'audio'
-//   ];
   let contexts = [
     'selection'
   ];
@@ -201,45 +166,4 @@ chrome.runtime.onInstalled.addListener(function () {
       id: context
     });
   }
-
-//   // Create a parent item and two children.
-//   let parent = chrome.contextMenus.create({
-//     title: 'Test parent item',
-//     id: 'parent'
-//   });
-//   chrome.contextMenus.create({
-//     title: 'Child 1',
-//     parentId: parent,
-//     id: 'child1'
-//   });
-//   chrome.contextMenus.create({
-//     title: 'Child 2',
-//     parentId: parent,
-//     id: 'child2'
-//   });
-
-//   // Create a radio item.
-//   chrome.contextMenus.create({
-//     title: 'radio',
-//     type: 'radio',
-//     id: 'radio'
-//   });
-
-//   // Create a checkbox item.
-//   chrome.contextMenus.create({
-//     title: 'checkbox',
-//     type: 'checkbox',
-//     id: 'checkbox'
-//   });
-
-//   // Intentionally create an invalid item, to show off error checking in the
-//   // create callback.
-//   chrome.contextMenus.create(
-//     { title: 'Oops', parentId: 999, id: 'errorItem' },
-//     function () {
-//       if (chrome.runtime.lastError) {
-//         console.log('Got expected error: ' + chrome.runtime.lastError.message);
-//       }
-//     }
-//   );
 });
